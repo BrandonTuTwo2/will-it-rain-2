78ebe04c4dc24cc0a1ca5455e08cac4e2571941f
Brunhilde <brandontu35@gmail.com>
2024-04-24 18:07:43 -0400

    feat added background and search bar and tailwind

---
+++ src/components/weatherSearch.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+import SearchInput from 'vue-search-input'
+import 'vue-search-input/dist/styles.css'
+
+const searchVal = ref('')
+</script>
+
+<template>
+    <div class="searchbar my-4">
+        <SearchInput v-model="searchVal" :placeholder="'Enter in the address you would like to check for rain'"/>
+    </div>
+    <button class="btn btn-primary btn-search" @click="console.log(searchVal); console.log('hi me');">Check</button>
+</template>
+
+<style scoped></style>
