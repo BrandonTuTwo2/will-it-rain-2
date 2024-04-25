5f30542d582263b1fd7959844999a9cfa380e8f7
Brunhilde <brandontu35@gmail.com>
2024-04-25 15:50:38 -0400

    does this work on prod?

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -1,12 +1,19 @@
 import express, { Router } from "express";
 import serverless from "serverless-http";
 
+require('dotenv').config()
+
 const api = express();
 const router = Router();
 
-router.get("/hello", (req, res) => res.send({
+
+
+router.get("/hello", (req, res) => {
+    console.log(process.env.test);
+    console.log("HI ME")
+    res.send({
     hi: "Mom"
-}));
+})});
 
 api.use("/api/", router);
 export const handler = serverless(api);
