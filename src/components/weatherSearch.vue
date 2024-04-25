d2aa241a6852a4c6d21635219de337c3b453f405
Brunhilde <brandontu35@gmail.com>
2024-04-25 14:28:02 -0400

    testing to see if this works on production

--- src/components/weatherSearch.vue
+++ src/components/weatherSearch.vue
@@ -5,13 +5,21 @@ import SearchInput from 'vue-search-input'
 import 'vue-search-input/dist/styles.css'
 
 const searchVal = ref('')
+
+const testHI = async () =>{
+    const res = await fetch('/api/hello');
+    const restTest = await res.json();
+    console.log(restTest);
+    //hdfd
+}
+
 </script>
 
 <template>
     <div class="searchbar my-4">
         <SearchInput v-model="searchVal" :placeholder="'Enter in the address you would like to check for rain'"/>
     </div>
-    <button class="btn btn-primary btn-search" @click="console.log(searchVal); console.log('hi me');">Check</button>
+    <button class="btn btn-primary btn-search" @click="testHI()">Check</button>
 </template>
 
 <style scoped></style>
