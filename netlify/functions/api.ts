f66c2a23011aa691af1a1f69d271f67f583d2a33
Brunhilde <brandontu35@gmail.com>
2024-05-01 18:21:27 -0400

    feat used the openAPI geo and weather forecast api and sent it to the front

--- netlify/functions/api.ts
+++ netlify/functions/api.ts
@@ -12,8 +12,36 @@ router.get("/hello", (req, res) => {
     console.log(process.env.test);
     console.log("HI ME")
     res.send({
+    statusCode:200,
     hi: "Mom"
 })});
+
+router.post("/getForecast", async (req,res) => {
+    //need to set lat and lon first
+    let status = 400
+    let lat = 0;
+    let lon = 0
+    let returnVal = "";
+    const address = JSON.parse(req.body);
+    const latLonRaw = await fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${address._value}&limit=5&appid=${process.env.API_KEY}`)
+    const latLon = await latLonRaw.json()
+
+    //If the location exist/is valid then we get 5 day weather forcast, if not then we return a 400
+    if(latLon) {
+        const location = latLon[0]
+        lat = location.lat
+        lon = location.lon
+        const weatherRaw = await fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}&units=metric`)
+        const weather = await weatherRaw.json()
+        status = 200
+        console.log(weather)    
+        returnVal = JSON.stringify(weather.list);
+    } 
+    res.send({
+        statusCode:status,
+        forecast : returnVal
+    })
+})
 
 api.use("/api/", router);
 export const handler = serverless(api);
--- src/components/weatherSearch.vue
+++ src/components/weatherSearch.vue
@@ -6,20 +6,31 @@ import 'vue-search-input/dist/styles.css'
 
 const searchVal = ref('')
 
-const testHI = async () =>{
-    const res = await fetch('/api/hello');
+const checkWeather = async () => {
+    const address = {
+        address: searchVal
+    }
+
+    const res = await fetch('/api/getForecast', {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(searchVal),
+    });
+    
     const restTest = await res.json();
-    console.log(restTest);
-    //hdfd
+    const forecast  = JSON.parse(restTest.forecast);
+    console.log(forecast);
 }
 
 </script>
 
 <template>
     <div class="searchbar my-4">
-        <SearchInput v-model="searchVal" :placeholder="'Enter in the address you would like to check for rain'"/>
+        <SearchInput v-model="searchVal" :placeholder="'<Your address here> no I won\'t steal it'" />
     </div>
-    <button class="btn btn-primary btn-search" @click="testHI()">Check</button>
+    <button class="btn btn-primary btn-search" @click="checkWeather()">Check</button>
 </template>
 
 <style scoped></style>
